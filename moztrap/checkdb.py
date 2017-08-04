d69a219e2b66bac8f02e0a4ac2291e7997a5bc70
acmos-takano <takano@newbiz.acmos.jp>
2017-08-04 17:02:28 +0900

    db errchk

--- moztrap/checkdb.py
+++ moztrap/checkdb.py
@@ -9,12 +9,17 @@ DB_PASS = os.getenv("MYSQL_ENV_MYSQL_PASS","000000")
 conn_string = ("dbname='" + DB_NAME + "' user='" + DB_USER + "' host='" + DB_HOST + "' password='" + DB_PASS + "'")
 print("Connecting to database:\n" + conn_string)
 
-con = MySQLdb.connect( user=DB_USER, passwd=DB_PASS, host=DB_HOST, db=DB_NAME)
-cur= con.cursor()
-
-cur.execute("select * from information_schema.tables where table_name=%s", ('django_admin_log',))
-exists = bool(cur.rowcount)
-
-if exists is False:
+try:
+  con = MySQLdb.connect( user=DB_USER, passwd=DB_PASS, host=DB_HOST, db=DB_NAME)
+  cur= con.cursor()
+  cur.execute("select * from information_schema.tables where table_name=%s", ('django_admin_log',))
+  exists = bool(cur.rowcount)
+  if exists is False:
+      print("Table does not appear to be setup.")
+      sys.exit(2)
+except Exception as e:
     print("Database does not appear to be setup.")
     sys.exit(2)
+finally:
+    cur.close()
+    con.close()
